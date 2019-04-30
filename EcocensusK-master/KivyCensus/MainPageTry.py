caf02a64a1001d830da7e55c281db67151dc09d6
The-Black_Star <32356221+The-Black-Star@users.noreplay.github.com>
2019-04-29 21:24:15 -1000

    made image list and fixed on press and on release

--- EcocensusK-master/KivyCensus/MainPageTry.py
+++ EcocensusK-master/KivyCensus/MainPageTry.py
@@ -12,11 +12,39 @@ import image_partition as impa
 import predictions as predict
 import imageReader as imread
 
+
+class bidirectional_iterator(object):
+    def __init__(self, collection):
+        self.collection = collection
+        self.index = 0
+
+    def next(self):
+        try:
+            result = self.collection[self.index]
+            self.index += 1
+        except IndexError:
+            raise StopIteration
+        return result
+
+    def prev(self):
+        self.index -= 1
+        if self.index < 0:
+            raise StopIteration
+        return self.collection[self.index]
+
+    def __iter__(self):
+        return self
+
+
 class Layout(FloatLayout):
+        
+        self.imglist
+    
     def selectDirectory(self):
         Tk().withdraw()
         self.folder.text = filedialog.askdirectory()
         # need to figure out how kivys file thing works
+    
     def Predict(self):
         Folder = self.folder.text
         altitude = float(self.altitude.text)
@@ -26,6 +54,20 @@ class Layout(FloatLayout):
         imread.main(Folder, altitude)
         partdirectory = os.path.dirname(Folder + '/Partitions/')
         os.rmdir(partdirectory)
+
+    def Imglist(self):
+        directory = os.listdir(self.folder.text)
+        imglist = []
+        for file in directory:
+            if ".JPG" in file or ".jpg" in file:
+                imglist.append(file)
+        self.imglist = bidirectional_iterator(imglist)
+    
+    def next(self):
+        return imglist.next()
+    
+    def prev(self):
+        return imglist.prev()
 
 class EcoCensus(App):
 
